ad5e62b1f5a8d6624ac4816a4a32c5142901ac52
tessereth <tessereth@yahoo.com.au>
2020-10-03 15:40:08 +1000

    Make the hero text the page title.

--- app/javascript/util/Hero.js
+++ app/javascript/util/Hero.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React from 'react'
+import { Helmet } from 'react-helmet'
 
-const Hero = ({ title, subtitle }) => {
+const Hero = ({ title, subtitle, pageTitle }) => {
   return (
     <section className="hero is-primary">
+      <Helmet>
+        <title>{pageTitle || title}</title>
+      </Helmet>
       <div className="hero-body">
         <div className="container">
           <h1 className="title">{title}</h1>
