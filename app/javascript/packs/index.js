61dd10a503837021ac8eec9953395912456aaf82
tessereth <tessereth@yahoo.com.au>
2020-10-04 14:09:15 +1100

    Delete unused files.

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,10 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "../../css/application.scss"
 import App from "../App"
-//import registerServiceWorker from "../registerServiceWorker"
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById("root"))
 })
-// TODO: Decide if we want service workers
-//registerServiceWorker()
