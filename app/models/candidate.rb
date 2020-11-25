14e888fa7a0f8839f2865c58b1de76eac1b2f30f
tessereth <tessereth@yahoo.com.au>
2020-11-25 21:44:29 +1100

    Add model validations.

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,6 @@
 class Candidate < ApplicationRecord
   belongs_to :position
   has_many :candidate_votes
+
+  validates :name, presence: true
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,4 +1,7 @@
 class Position < ApplicationRecord
   belongs_to :election
   has_many :candidates
+
+  validates :name, presence: true
+  validates :seats, numericality: { only_integer: true, greater_than: 0 }
 end
